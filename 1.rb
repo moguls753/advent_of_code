182460ca6ce303f370d96d8262931e47e98c3994
Eike <eike.rackwitz@mail.de>
2024-12-03 22:35:37 +0100

    day1 solution

---
+++ 1.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+file = File.open('1_input')
+array1 = []
+array2 = []
+file.each_line do |line|
+  a, b = line.split
+  array1 << a.to_i
+  array2 << b.to_i
+end
+array1.sort!
+array2.sort!
+result = 0
+array1.each_with_index do |elem, index|
+  result += (elem.to_i - array2[index].to_i).abs
+end
+puts result
---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem "ruby-lsp", "~> 0.22.1"
+
+gem "rubocop", "~> 1.69"
